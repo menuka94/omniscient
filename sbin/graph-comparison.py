17a728eef4446f2a1f3087bd34db1f0ca186ace8
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2021-07-02 01:20:55 -0600

    implement graph-comparison - 2

--- sbin/graph-comparison.py
+++ sbin/graph-comparison.py
@@ -9,18 +9,16 @@ import matplotlib.pyplot as plt
 import matplotlib
 import re
 
-# stats_root_dir = sys.argv[1]
+stats_root_dir = sys.argv[1]
 
-spark_root_dir = "/s/lattice-150/a/all/all/all/sustain/omniscient/data/spark-kmeans-svi-county"
-k8s_root_dir = "/s/lattice-150/a/all/all/all/sustain/omniscient/data/k8s-kmeans-svi-county"
-graphs_root_dir = f'{spark_root_dir}/graphs'
+spark_root_dir = f'{stats_root_dir}/spark'
+k8s_root_dir = f'{stats_root_dir}/k8s'
+graphs_root_dir = f'{stats_root_dir}'
 
 
 def rename_and_get_sorted_file_names(root_dir):
     # list of all file names
     list_dir = os.listdir(root_dir)
-    if 'env' in list_dir:
-        list_dir.remove('env')
     list_dir.remove('aggregate.nmon.csv')
 
     # Pad File number with zero if the number of digits is 1
@@ -43,9 +41,6 @@ def rename_and_get_sorted_file_names(root_dir):
         elif file_name.find(".csv") == -1:
             list_dir.remove(file_name)
 
-    if 'env' in list_dir:
-        list_dir.remove('env')
-
     return sorted(list_dir)
 
 
@@ -62,6 +57,7 @@ class MachineAverage:
     """
     To represent average average resource utilization metrics for each host
     """
+
     def __init__(self, hostname, cpu, memory, network_write, network_read):
         self.hostname = hostname
         self.cpu = cpu
@@ -99,4 +95,67 @@ def get_averages_map(file_names, root_dir):
 # Create Maps of average resource utilization values
 spark_averages = get_averages_map(spark_file_names, spark_root_dir)
 k8s_averages = get_averages_map(k8s_file_names, k8s_root_dir)
+
+# create lists for graphing
+spark_hostnames = [machine.hostname for _, machine in spark_averages.items()]
+spark_cpu_values = [machine.cpu for _, machine in spark_averages.items()]
+spark_memory_values = [machine.memory for _, machine in spark_averages.items()]
+spark_network_write_values = [machine.network_write for _, machine in spark_averages.items()]
+spark_network_read_values = [machine.network_read for _, machine in spark_averages.items()]
+
+k8s_hostnames = [machine.hostname for _, machine in k8s_averages.items()]
+k8s_cpu_values = [machine.cpu for _, machine in k8s_averages.items()]
+k8s_memory_values = [machine.memory for _, machine in k8s_averages.items()]
+k8s_network_write_values = [machine.network_write for _, machine in k8s_averages.items()]
+k8s_network_read_values = [machine.network_read for _, machine in k8s_averages.items()]
+
+# Graph
+# update graph fonts
+font = {
+    'weight': 'bold',
+    'size': 16
+}
+matplotlib.rc('font', **font)
+
+plt1 = plt.figure(figsize=(15, 20))
+plt.subplot(4, 1, 1)
+plt.xlabel('Machine')
+plt.ylabel('MEM:active')
+plt.xticks(rotation=90, ha='right')
+plt.plot(spark_hostnames, spark_memory_values, 'r', label="Bare-Metal")
+plt.plot(k8s_hostnames, k8s_memory_values, 'b', label="K8s")
+plt.legend()
+plt.title("Memory")
+
+plt.subplot(4, 1, 2)
+plt.xlabel('Machine')
+plt.ylabel('CPU Usage (%)')
+plt.xticks(rotation=90, ha='right')
+plt.plot(spark_hostnames, spark_cpu_values, 'r', label="Bare-Metal")
+plt.plot(k8s_hostnames, k8s_cpu_values, 'b', label="K8s")
+plt.legend()
+plt.title('CPU Usage (%)')
+
+plt.subplot(4, 1, 3)
+plt.xlabel('Machine')
+plt.ylabel('NET:eno1-write-KB/s')
+plt.xticks(rotation=90, ha='right')
+plt.plot(spark_hostnames, spark_network_write_values, 'r', label="Bare-Metal")
+plt.plot(k8s_hostnames, k8s_network_write_values, 'b', label="K8s")
+plt.legend()
+plt.title('Network Write')
+
+plt.subplot(4, 1, 4)
+plt.xlabel('Machine')
+plt.ylabel('NET:eno1-read-KB/s')
+plt.xticks(rotation=90, ha='right')
+plt.plot(spark_hostnames, spark_network_read_values, 'r', label="Bare-Metal")
+plt.plot(k8s_hostnames, k8s_network_read_values, 'b', label="K8s")
+plt.legend()
+plt.title('Network Read')
+
+plt.tight_layout()
+plt.savefig(f'{graphs_root_dir}/comparison.png', dpi=300)
+# plt.show()
+print('[+] Comparison Graph Saved')
 
